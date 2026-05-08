7a3f66c9040b610c6d00aba593edfb7ff4dc41a7
Youness Ben <tudorbenhadda@gmail.com>
2026-05-08 15:47:51 +0100

    Fully Wroking version 1.1

--- animal_feed_optimizer/data.py
+++ animal_feed_optimizer/data.py
@@ -3,23 +3,27 @@ ingredients = {
     'mais': {
         "cost": 4,
         "protein": 0.9,
+        "energy": 3.5,
         "max_stock":90
     },
 
     'orge': {
         "cost": 3,
         "protein": 0.06,
+        "energy": 2.8,
         "max_stock": 20
     },
 
     'soja': {
         "cost": 6,
         "protein": 0.50,
+        "energy":4.2,
         "max_stock": 70
     }
 }
 
 requirements = {
     "total_weight": 100,
+    "min_energy": 4.2,
     "min_protein": 18
 }
--- animal_feed_optimizer/optimizer.py
+++ animal_feed_optimizer/optimizer.py
@@ -9,32 +9,33 @@ def optimize_feed():
     for i in ingredients:
         variables[i] = LpVariable(i, lowBound=0)
 
-    # Objective: minimize cost
     problem += lpSum(
         ingredients[i]["cost"] * variables[i]
         for i in ingredients
     )
 
-    # Total weight constraint
     problem += lpSum(
         variables[i]
         for i in ingredients
     ) <= requirements["total_weight"]
 
-    # Protein constraint (IMPORTANT FIX: >=)
     problem += lpSum(
         ingredients[i]["protein"] * variables[i]
         for i in ingredients
     ) >= requirements["min_protein"]
 
-    # Stock constraints
+    problem += lpSum(
+        ingredients[i]["energy"] * variables[i]
+        for i in ingredients
+    ) >= requirements["min_energy"]
+
+    problem += variables["soja"] >= 5
+
     for i in ingredients:
         problem += variables[i] <= ingredients[i]["max_stock"]
 
-    # Solve ONCE
     problem.solve()
 
-    # Results
     results = {}
 
     for i in ingredients:
