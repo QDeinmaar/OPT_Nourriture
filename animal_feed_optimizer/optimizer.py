d200b003403ae9f4c3ab869628a7dbdcca107ec0
Youness Ben <tudorbenhadda@gmail.com>
2026-05-08 15:40:40 +0100

    Fully Working version 1.0

--- animal_feed_optimizer/optimizer.py
+++ animal_feed_optimizer/optimizer.py
@@ -2,39 +2,41 @@ from pulp import *
 from data import ingredients, requirements
 
 def optimize_feed():
-    problem = LpProblem( "Animal_feed_optimizer", LpMinimize)
+    problem = LpProblem("Animal_feed_optimizer", LpMinimize)
 
     variables = {}
 
-    for ingredient in ingredients:
-        variables[ingredient] = LpVariable(
-            ingredient,
-            lowBound = 0
-        )
+    for i in ingredients:
+        variables[i] = LpVariable(i, lowBound=0)
 
+    # Objective: minimize cost
     problem += lpSum(
         ingredients[i]["cost"] * variables[i]
         for i in ingredients
     )
 
+    # Total weight constraint
     problem += lpSum(
-        variables[i] for i in ingredients
+        variables[i]
+        for i in ingredients
     ) <= requirements["total_weight"]
 
+    # Protein constraint (IMPORTANT FIX: >=)
     problem += lpSum(
-        variables[i]["protein"] * variables[i]
+        ingredients[i]["protein"] * variables[i]
         for i in ingredients
     ) >= requirements["min_protein"]
 
+    # Stock constraints
     for i in ingredients:
-        problem += (
-            variables[i] <= ingredients[i]["max_stock"]
-        )
+        problem += variables[i] <= ingredients[i]["max_stock"]
 
-        problem.solve()
+    # Solve ONCE
+    problem.solve()
 
-        results = {}
-    
+    # Results
+    results = {}
+
     for i in ingredients:
         results[i] = value(variables[i])
 
