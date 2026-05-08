083e892209dee90b2bcc30039009b720d94ea349
Youness Ben <tudorbenhadda@gmail.com>
2026-05-08 15:07:13 +0100

    Added data.py

---
+++ animal_feed_optimizer/data.py
@@ -0,0 +1,25 @@
+
+ingredients = {
+    'mais': {
+        "cost": 4,
+        "protein": 0.9,
+        "max_stock":90
+    },
+
+    'orge': {
+        "cost": 3,
+        "protein": 0.06,
+        "max_stock": 20
+    },
+
+    'soja': {
+        "cost": 6,
+        "protein": 0.50,
+        "max_stock": 70
+    }
+}
+
+requirement = {
+    "total_weight": 100,
+    "min_protein": 18
+}
